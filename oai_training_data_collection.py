705a0d2a37838e16166d6d231aba799720d05539
bayesmaxxing <bayesmaxxing@gmail.com>
2024-05-28 19:10:53 +0200

    training data script

--- oai_training_data_collection.py
+++ oai_training_data_collection.py
@@ -3,6 +3,27 @@ import numpy as np
 from openai import OpenAI
 
 client = OpenAI()
+project_id = 'proj_5JF0o5O7ktRRByRy75gKMxf9'
+
+# Load prompts here
+models = ["gpt-4o", "gpt-3.5-turbo"]
+prompts = np.read_csv()
+
+message = []
+model_list = [] 
+
+for model in models:
+    for prompt in prompts:
+    
+        # Call to OAI API
+        completion = client.chat.completions.create(
+            model = "gpt-4o", 
+            messages = [
+                {"role": "system", "content": "You are a helpful assistant."}, 
+                {"role":"user", "content":prompt}
+            ]
+        )
+        message.append(completion.choices[0].message)
+        model_list.append(model)
 
 
-# create a dataframe to be 
